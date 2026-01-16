f6d135c15835abad454addbb3346a9206ec9c68f
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-16 23:50:55 +0100

    Feat : added functions to get the existing supplier, update it, and delete it

--- src/app/api/supplier.service.ts
+++ src/app/api/supplier.service.ts
@@ -2,13 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Supplier } from './supplier.model';
+import { enviroment } from '../../enviroments/enviroment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupplierService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/api/v1/suppliers';
+  private apiUrl = `${enviroment.apiUrl}/suppliers` ;
 
   //  Return Observable<Supplier[]> (Array) instead of Page
   getSuppliers(page: number = 0, size: number = 10): Observable<Supplier[]> {
@@ -19,7 +20,21 @@ export class SupplierService {
     return this.http.get<Supplier[]>(this.apiUrl, { params });
   }
 
+  //create the supplier
   createSupplier(supplier: Partial<Supplier>): Observable<Supplier> {
     return this.http.post<Supplier>(this.apiUrl, supplier);
+  }
+
+  getSupplierById(id: number): Observable<Supplier> {
+    return this.http.get<Supplier>(`${this.apiUrl}/${id}`);
+  }
+
+  updateSupplier(id: number, supplier: Partial<Supplier>): Observable<Supplier> {
+    return this.http.put<Supplier>(`${this.apiUrl}/${id}`, supplier);
+  }
+
+  //delete the supplier 
+  deleteSupplier(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
