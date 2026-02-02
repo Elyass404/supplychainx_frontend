292fb93feda64d30a8053f3db749682490034c84
ilyass mar <ilyass.marghine22@gmail.com>
2026-02-02 23:39:24 +0100

    added the route for the new raw material and the update raw material

--- src/app/features/procurement/procurement.routes.ts
+++ src/app/features/procurement/procurement.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SupplierList } from './supplier-list/supplier-list';
 import { SupplierForm } from './supplier-form/supplier-form';
+import { RawMaterialList } from './raw-material-list/raw-material-list';
+import { RawMaterialForm} from './raw-material-form/raw-material-form'; 
 
 export const PROCUREMENT_ROUTES: Routes = [
 
@@ -17,6 +19,26 @@ export const PROCUREMENT_ROUTES: Routes = [
   {
     path: 'suppliers/:id/edit',
     component: SupplierForm
+  },
+
+  //raw materials routes
+
+  //Create new raw material
+  { 
+    path: 'raw-materials/new', 
+    component: RawMaterialForm
+  },
+
+  //Edit route
+  { 
+    path: 'raw-materials/:id/edit', 
+    component: RawMaterialForm 
+  },
+
+  //Show the list of raw materials
+  {
+    path: 'raw-materials',
+    component: RawMaterialList
   },
   
   // Default: If they just go to /procurement, redirect to suppliers
