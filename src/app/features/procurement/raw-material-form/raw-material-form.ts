7402498dfa0ee3b8fcb633e8e49904a45312b4e0
ilyass mar <ilyass.marghine22@gmail.com>
2026-02-02 23:40:54 +0100

    added the page for the raw material addition form

---
+++ src/app/features/procurement/raw-material-form/raw-material-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raw-material-form',
+  imports: [],
+  templateUrl: './raw-material-form.html',
+  styles: ``,
+})
+export class RawMaterialForm {
+
+}
