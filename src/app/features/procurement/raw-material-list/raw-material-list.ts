e2f2288a37aefe42203181e4482d63a038716324
ilyass mar <ilyass.marghine22@gmail.com>
2026-02-03 22:35:29 +0100

    added the functionality of delete the raw materials, but still working on the update

--- src/app/features/procurement/raw-material-list/raw-material-list.ts
+++ src/app/features/procurement/raw-material-list/raw-material-list.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';   // Needed for navigation buttons
 import { RawMaterialService } from '../../../api/raw-material.service';
 import { RawMaterial } from '../../../api/raw-material-model';
 
-// 🟢 DECORATOR: @Component
+// DECORATOR: @Component
 // This tells Angular: "This class controls a part of the screen."
 @Component({
   selector: 'app-raw-material-list',
@@ -15,7 +15,7 @@ import { RawMaterial } from '../../../api/raw-material-model';
 })
 export class RawMaterialList implements OnInit {
 
-  // 🟢 DEPENDENCY INJECTION (Modern Style)
+  // DEPENDENCY INJECTION (Modern Style)
   // Instead of the constructor, we use 'inject()'. It's cleaner.
   private materialService = inject(RawMaterialService);
 
@@ -30,7 +30,7 @@ export class RawMaterialList implements OnInit {
   // 3. The Filter Switch (True = Show only critical, False = Show all)
   showCriticalOnly = signal<boolean>(false);
 
-  // 🟢 COMPUTED SIGNAL
+  // COMPUTED SIGNAL
   // This is magic. It automatically recalculates whenever 'materials' OR 'showCriticalOnly' changes.
   // We don't need to manually filter inside a function. Angular does it for us.
   filteredMaterials = computed(() => {
@@ -71,5 +71,26 @@ export class RawMaterialList implements OnInit {
   toggleFilter() {
     // .update() takes the current value and flips it
     this.showCriticalOnly.update(value => !value);
+  }
+
+  // DELETE FUNCTION
+  onDelete(material: RawMaterial) {
+    // Confirm with user
+    const confirmed = window.confirm(`Are you sure you want to delete ${material.name}?`);
+    
+    if (confirmed) {
+      // Call Service
+      this.materialService.delete(material.id).subscribe({
+        next: () => {
+          console.log('Deleted successfully');
+          // Remove item from the signal immediately (Updates UI instantly)
+          this.materials.update(current => current.filter(m => m.id !== material.id));
+        },
+        error: (err) => {
+          console.error('Delete failed', err);
+          alert('Could not delete material. It might be used in production.');
+        }
+      });
+    }
   }
 }
