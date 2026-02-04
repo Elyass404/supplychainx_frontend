053e329b04086cf3a52e6c9c49f51131a64b5a74
ilyass mar <ilyass.marghine22@gmail.com>
2026-02-04 23:07:31 +0100

    added the path for the routes of production in the app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 import { AppLayout } from './core/layout/app-layout/app-layout';
 import { Login } from './features/auth/login';
 import { authGuard } from './core/guards/auth.guard'; 
+
 import { PROCUREMENT_ROUTES } from './features/procurement/procurement.routes'; 
+
+import { PRODUCTION_ROUTES } from './features/production/production.routes';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
@@ -19,7 +22,14 @@ export const routes: Routes = [
       {
         path: 'procurement',
         loadChildren: () => import('./features/procurement/procurement.routes').then(m => m.PROCUREMENT_ROUTES)
-      }
+      },
+      
+      //if the the path starts with production, then look in the productin routes
+      {
+        path: 'production',
+        loadChildren: () => import('./features/production/production.routes')
+          .then(m => m.PRODUCTION_ROUTES)
+      },
     ]
   },
 
