265bcaf3be564bd7e557a649dbed11a8d1c7ada4
ilyass mar <ilyass.marghine22@gmail.com>
2026-02-06 23:46:36 +0100

    added the imports of the ngrx and configured the store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, importProvidersFrom, APP_INITIALIZER } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, APP_INITIALIZER, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { AuthService } from './core/auth/auth';
+import { CustomerEffects } from './features/delivery/store/customer.effects';
+
+//  NgRx Imports
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { customerReducer } from './features/delivery/store/customer.reducer';
 
 // this function triggers the login check
 function initializeApp(authService: AuthService): () => Promise<void> {
@@ -20,7 +27,22 @@ export const appConfig: ApplicationConfig = {
         sendAccessToken: true
       }
     })),
-    //PROVIDER: Use the factory we created above
+
+    // Register the Store (Reducer)
+    provideStore({
+      customer: customerReducer
+    }),
+
+    // Register Effects (Currently empty, we will fill this in Block 3)
+    provideEffects([CustomerEffects]),
+
+    // Enable DevTools (Great for debugging in Chrome)
+    provideStoreDevtools({
+      maxAge: 25, 
+      logOnly: !isDevMode()
+    }),
+
+    // PROVIDER: Auth Initializer
     {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
